fb8b6fbfb24ca2255ddcbe7e5c0a89cb11ed4873
korat-krish <krishkorat205@gmail.com>
2025-10-02 15:55:36 +0530

    Update cart.js

--- js/cart.js
+++ js/cart.js
@@ -1,35 +1,53 @@
+
 Display = () => {
     let cartRec = JSON.parse(localStorage.getItem('Cartitem')) || [];
     let str = ''
-    let data1 = document.querySelector('#row1');
+    let container = document.querySelector('#cartItems');
 
-
-    let qty = 1;
-    
     cartRec.forEach((ele, index) => {
         str += `
-        <tr>
-        <div class="d-flex bg-white mt-2">
-            <td class="col-3">
-                <img src="${ele.thumbnail}" class="img-fluid w-25" alt="image1"/>
-            </td>
-            <td class="d-flex justify-content-center align-items-center">
-                <h5 class="col-3 text-center  d-flex justify-content-center align-items-center">${ele.title}</h5>
-            </td>
-            <td class="col-2">
-                <h5 class="col-2 text-center  d-flex justify-content-center align-items-center">${qty}</h5>
-            </td>
-            <td>
-                <h5 class="col-1  d-flex justify-content-center align-items-center">${ele.price}</h5>
-            </td>
-            <td>
-            <h5 class="col-3 d-flex justify-content-center align-items-center">${ele.price * qty}</h5> 
-            </td>
-            </div>
-        </tr>
+        <div class="cart-item">
+        <img src="${ele.thumbnail}" alt="Product">
+        <div class="item-info">
+          <h6>${ele.title}</h6>
+          
+        </div>
+        <div class="item-price">$${ele.price}</div>
+        <div class="item-qty">
+          <button >-</button>
+          <span class="cqty">${ele.qty}</span>
+          <button onclick="increse(${index})">+</button>
+        </div>
+        <div class="item-total">$${ele.price * ele.qty}</div>
+        <div class="item-remove">
+          <button>🗑</button>
+        </div>
+      </div>
         `;
     });
-    data1.innerHTML = str;
+    container.innerHTML = str;
 
 }
 Display();
+
+function duplicate(id) {
+
+    let cart = JSON.parse(localStorage.getItem("Cartitem")) || [];
+
+    if (cart.find((element) => (element.id == id))) {
+        if (cart.map((element) => (element.id == id))) {
+            cart.qty += 1;
+            localStorage.setItem("Cartitem", JSON.stringify(cart));
+
+        }
+        else {
+            cart.qty = 1;
+            localStorage.setItem("Cartitegitm", JSON.stringify(cart));
+        }
+    }
+    else {
+        cart.qty = 1;
+        localStorage.setItem("Cartitem", JSON.stringify(cart));
+    }
+    Display();
+}
